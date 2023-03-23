6f46da393cecc8b8c8196c72a18d416dbd22b60e
= <=>
2023-03-23 09:19:01 -0400

    Experienced year updated

--- src/components/hero/content.js
+++ src/components/hero/content.js
@@ -4,7 +4,7 @@ export const heroContent = {
   heroTitleName: "Kevin Harris",
   heroDesignation: "Full-Stack Blockchain Developer",
   heroDescriptions: `I am an experienced senior full-stack developer who has been mainly focusing on JavaScript, Python, C/C++ and Java to develop the products.
-  During the last 6+ years, I have worked in many companies to build applications for people all over the world. As an active and innovative learner, I love learning new programming languages and skills. 
+  During the last 8+ years, I have worked in many companies to build applications for people all over the world. As an active and innovative learner, I love learning new programming languages and skills. 
   I have rich experience in blockchain technology and cryptocurrencies  as well for DeFi application. I fully guarantee my code quality and I am eager to brainstorm a new idea with teammates.
   My goal is to offer more products that people would prefer.`,
   heroBtn: "more about me",
